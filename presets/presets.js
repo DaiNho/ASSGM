db52882cd645cc7c351cd1eda706ea2c67d74f8c
truongbabiboi1204 <Caovantruong2503@gmail.com>
2025-07-13 21:50:40 +0700

    add img

--- presets/presets.js
+++ presets/presets.js
@@ -157,4 +157,317 @@ export const presets = [
       { x: 180, y: 40, width: 120, height: 160 }
     ],
   },
+  {
+    id: 15,
+    label: "Khung dễ thương",
+    image: require("../assets/15.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 16,
+    label: "Khung cơ bản đen",
+    image: require("../assets/16.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 17,
+    label: "Khung dễ thương",
+    image: require("../assets/17.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 18,
+    label: "Khung dễ thương",
+    image: require("../assets/18.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 19,
+    label: "Khung cơ bản",
+    image: require("../assets/19.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 20,
+    label: "Khung cơ bản đen",
+    image: require("../assets/20.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 21,
+    label: "Khung cơ bản đỏ",
+    image: require("../assets/21.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 22,
+    label: "Khung cơ bản đỏ",
+    image: require("../assets/22.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 23,
+    label: "Khung cơ bản ",
+    image: require("../assets/23.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 24,
+    label: "Khung cơ bản ",
+    image: require("../assets/24.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 25,
+    label: "Khung dễ thương ",
+    image: require("../assets/25.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 26,
+    label: "Khung cơ bản ",
+    image: require("../assets/26.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 27,
+    label: "Khung cơ bản đen",
+    image: require("../assets/27.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 28,
+    label: "Khung cơ bản đen",
+    image: require("../assets/27.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 29,
+    label: "Khung dễ thương",
+    image: require("../assets/27.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 30,
+    label: "Khung cơ bản đỏ",
+    image: require("../assets/30.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 31,
+    label: "Khung cơ bản đen",
+    image: require("../assets/31.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 32,
+    label: "Khung cơ bản đen",
+    image: require("../assets/32.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 33,
+    label: "Khung dễ thương",
+    image: require("../assets/33.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 34,
+    label: "Khung dễ thương",
+    image: require("../assets/34.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 35,
+    label: "Khung dễ thương",
+    image: require("../assets/35.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 }
+    ],
+  },
+  {
+    id: 36,
+    label: "Khung dễ thương",
+    image: require("../assets/36.png"),
+    originalWidth: 400,
+    originalHeight: 600,
+    slots: [
+      { x: 30, y: 40, width: 120, height: 160 },
+      { x: 180, y: 40, width: 120, height: 160 },
+      { x: 30, y: 40, width: 120, height: 160 },
+    ],
+  },
 ];
